b0fed966de71ff29e50fcfe27e5d4989e8a3c207
Roman Kh <rah83000@gmail.com>
2024-10-20 12:42:51 +0200

    added player (triangle)

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import * 
+from player import * 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    Clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
         screen.fill("black")
+        player.draw(screen)        
         pygame.display.flip()
+        dt = Clock.tick() / 1000
+        
 
 
 if __name__ == "__main__":
