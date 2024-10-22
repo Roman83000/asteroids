1bc4fc89d4dab32dc3f5b6f1d798b2f14c7c43d6
Roman Kh <rah83000@gmail.com>
2024-10-22 14:42:27 +0200

    added move and asteroids with asteroidsField

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import pygame
 from constants import * 
 from player import * 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        
+    
     dt = 0
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+
+    AsteroidField.containers = (updatable)
+    asterfield = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)        
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)   
+
         pygame.display.flip()
         dt = Clock.tick() / 1000
         
