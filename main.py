be546974b32f5933f72ddc17e27f3dc50d394cd1
Roman Kh <rah83000@gmail.com>
2024-10-24 17:00:20 +0200

    added shooting and shoot cooldown

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import * 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -14,30 +15,35 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+    AsteroidField.containers = updatable
+    asterfield = AsteroidField()
 
     Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, shots)
     
     dt = 0
-    Asteroid.containers = (asteroids, updatable, drawable)
+    
 
 
-    AsteroidField.containers = (updatable)
-    asterfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+            
         for obj in updatable:
             obj.update(dt)
         
         for obj in asteroids:
-            if player.is_collide(obj) == True:
+            if player.is_collide(obj):
                 print("Game over!")
                 sys.exit()
-
 
 
         screen.fill("black")
